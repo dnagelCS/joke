f658a981c738dc7968e4a8c38b62a19d10ff467f
Devora Nagel <devoranagel@gmail.com>
2020-05-18 17:11:36 -0500

    created jokeFrame

--- src/main/java/nagel/joke/JokeController.java
+++ src/main/java/nagel/joke/JokeController.java
@@ -4,28 +4,29 @@ package nagel.joke;
 
 import retrofit2.*;
 
+import javax.swing.*;
+
 public class JokeController {
 
     private JokeService service;
-    private JokeView view;
-    private String type;
+    private JLabel joke;
 
-    public JokeController(JokeService service, JokeView view) {
+    public JokeController(JokeService service, JLabel joke) {
         this.service = service;
-        this.view = view;
+        this.joke = joke;
     }
 
-    public void requestData() {
-        service.getJoke(this.type).enqueue(new Callback<Joke>() {
+    public void requestData(String type) {
+        service.getJoke(type).enqueue(new Callback<Joke>() {
             @Override
             public void onResponse(Call<Joke> call, Response<Joke> response) {
                 Joke joke = response.body();
-                view.setJoke(joke);
+                assert joke != null;
             }
 
             @Override
             public void onFailure(Call<Joke> call, Throwable t) {
-
+                t.printStackTrace();
             }
         });
     }
--- src/main/java/nagel/joke/JokeFrame.java
+++ src/main/java/nagel/joke/JokeFrame.java
@@ -8,14 +8,51 @@ import java.awt.*;
 
 public class JokeFrame extends JFrame {
 
+    private JPanel promptPanel;
+    private JLabel prompt;
+
+    private JPanel typePanel;
+    private JButton general;
+    private JButton programming;
+    private JButton knockKnock;
+
+    private JPanel jokePanel;
+    private JLabel joke;
+
     public JokeFrame() {
         setSize(600, 400);
-        setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
         setTitle("Random Joke");
-        setLayout(new BorderLayout());
+        setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
 
-        JokeView view = new JokeView();
-        add(view, BorderLayout.CENTER);
+        promptPanel = new JPanel();
+        promptPanel.setLayout(new BorderLayout());
+        prompt = new JLabel();
+        prompt.setText("Choose a type of joke:");
+        promptPanel.add(prompt);
+
+        typePanel = new JPanel();
+        typePanel.setLayout(new BorderLayout());
+        general = new JButton("General");
+        general.addActionListener(actionEvent -> getJokeType());
+        programming = new JButton("Programming");
+        programming.addActionListener(actionEvent -> getJokeType());
+        knockKnock = new JButton("Knock-Knock");
+        knockKnock.addActionListener(actionEvent -> getJokeType());
+        typePanel.add(general);
+        typePanel.add(programming);
+        typePanel.add(knockKnock);
+
+        jokePanel = new JPanel();
+        jokePanel.setLayout(new BorderLayout());
+        joke = new JLabel();
+        jokePanel.add(joke);
+
+        add(promptPanel, BorderLayout.NORTH);
+        add(typePanel, BorderLayout.CENTER);
+        add(jokePanel, BorderLayout.SOUTH);
+    }
+
+    private void getJokeType() {
 
         Retrofit retrofit = new Retrofit.Builder()
                 .baseUrl("https://official-joke-api.appspot.com")
@@ -23,8 +60,21 @@ public class JokeFrame extends JFrame {
                 .build();
         JokeService service = retrofit.create(JokeService.class);
 
-        JokeController controller = new JokeController(service, view);
-        controller.requestData();
+        String type = null;
+        switch (type) {
+           case "General":
+               service.getJoke("general");
+               break;
+           case "Programming":
+               service.getJoke("programming");
+               break;
+           case "Knock-Knock":
+               service.getJoke("knock-knock");
+               break;
+       }
+
+        JokeController controller = new JokeController(service, joke);
+        controller.requestData(type);
     }
 
     public static void main(String[] args) {
